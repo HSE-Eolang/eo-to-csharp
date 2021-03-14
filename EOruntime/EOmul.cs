5a1892e16c0d6e7b6a417e0b91a758d7a5bd010c
Максим <kyngfy-panda@yandex.ru>
2021-03-15 00:22:18 +0300

    Add examples

---
+++ EOruntime/EOmul.cs
@@ -0,0 +1,17 @@
+﻿public class EOmul : EOobject
+{
+    public EOmul(dynamic left, dynamic right)
+    {
+        Left = left;
+        Right = right;
+    }
+
+    private dynamic Left { get; }
+
+    private dynamic Right { get; }
+
+    public override dynamic Φ()
+    {
+        return Left * Right;
+    }
+}
